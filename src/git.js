5dc23193f689355bece0acd82371e04f306d32cb
Steve King <steve@mydev.co>
2016-05-16 20:24:21 +0100

    Switch to using `trailingFunctionArgument` for getting callback

--- src/git.js
+++ src/git.js
@@ -661,31 +661,27 @@
    };
 
    /**
-    * Returns a list of objects in a tree based on commit hash. Passing in an object hash returns the object's content, size, and type.
+    * Returns a list of objects in a tree based on commit hash. Passing in an object hash returns the object's content,
+    * size, and type.
+    *
     * Passing "-p" will instruct cat-file to determine the object type, and display its formatted contents.
     *
-    *
-    * @param {string|string[]} [options]
+    * @param {string[]} [options]
     * @param {Function} [then]
     */
   Git.prototype.catFile = function (options, then) {
+     var handler = Git.trailingFunctionArgument(arguments);
      var command = ['cat-file'];
 
      if (typeof options === 'string') {
-        command[0] += ' ' + options;
-        this._getLog('warn',
-           'Git#diff: supplying options as a single string is now deprecated, switch to an array of strings');
+        throw new TypeError('Git#catFile: options must be supplied as an array of strings');
      }
      else if (Array.isArray(options)) {
         command.push.apply(command, options);
      }
 
-     if (typeof arguments[arguments.length - 1] === 'function') {
-        then = arguments[arguments.length - 1];
-     }
-
      return this._run(command, function (err, data) {
-        then && then(err, data);
+        handler && handler(err, data);
      });
   };
 
