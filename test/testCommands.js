c967dbdc35df0157ab7406e2e9b95b6d56c6e8ac
Steve King <steve@mydev.co>
2016-05-18 21:58:22 +0100

    Test for pull.

--- test/testCommands.js
+++ test/testCommands.js
@@ -776,6 +776,52 @@ exports.revParse = {
     }
 };
 
+exports.pull = {
+    setUp: function (done) {
+        Instance();
+        done();
+    },
+
+    'pulls with spaces in names': function (test) {
+        git.pull(function (err, result) {
+            test.same(['pull'], theCommandRun());
+            test.same(result.files.length, 21);
+            test.done();
+        });
+
+        closeWith('\n\
+From git.kellpro.net:apps/templates\n\
+* branch            release/0.33.0 -> FETCH_HEAD\n\
+Updating 1c6e99e..2a5dc63\n\
+Fast-forward\n\
+ accounting_core.kjs        |  61 +++++++++++-----------\n\
+ accounting_core_report.kjs |  45 +++++++++-------\n\
+ ap.invoice.kjs             |   2 +-\n\
+ ar.deposit.kjs             |   6 +--\n\
+ ar.invoice_detail.kjs      |  16 +++---\n\
+ ar.receipt.kjs             |  10 +++-\n\
+ gl.bank_statement.kjs      |   6 +++\n\
+ gl.kjs                     | 106 ++++++++++++++++++++++++++------------\n\
+ kis.call.kjs               |   2 +\n\
+ kis.call_stats_report.kjs  | 289 ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n\
+ kis.edit_recurring.kjs     |   8 +--\n\
+ kis.kdr_logs.kjs           |   8 ---\n\
+ kpo.batch_pay.kjs          |  19 ++++---\n\
+ kpo.fiscal_year.kjs        |  93 +++++++++++++++++++++++++++++----\n\
+ kpo.kjs                    |   2 +-\n\
+ kpo.payment.kjs            |   3 ++\n\
+ kpo.po_adjustment.kjs      |  82 +++++++++++++++++++++++------\n\
+ kpo.purchase_order.kjs     |  12 +++--\n\
+ kpo.reports.kjs            |  79 +++++++++++++++-------------\n\
+ kpo.warrant.kjs            |  17 +++---\n\
+ time_tracking.schedule.kjs | 342 +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++----------------------------------------------------------\n\
+ 21 files changed, 856 insertions(+), 352 deletions(-)\n\
+ create mode 100644 kis.call_stats_report.kjs\n\
+');
+
+    }
+};
+
 exports.show = {
     setUp: function (done) {
         sandbox.stub(console, 'warn');
