c162b93f5b68dad30a3f991fd72fd75ac0ae293d
Stephen Lautier <stephen.lautier@outlook.com>
2017-03-24 02:32:04 +0100

    feat(typings): initial typings for promise based api
    - added copy task for typings (not to bloat root)
    - npmignored tests

--- GruntFile.js
+++ GruntFile.js
@@ -17,18 +17,27 @@ module.exports = function (grunt) {
             commitMessage: 'Release <%= version %>', //default: 'release <%= version %>'
             tagMessage: 'Tag version <%= version %>' //default: 'Version <%= version %>'
          }
+      },
+      
+      copy: {
+        typings: {
+            files: [
+                { expand: true, cwd: './typings', src: ['./**'], dest: '.' }
+            ]
+        }  
       }
    });
 
    grunt.loadNpmTasks('grunt-contrib-nodeunit');
    grunt.loadNpmTasks('grunt-release-steps');
+   grunt.loadNpmTasks('grunt-contrib-copy');
 
    grunt.registerTask('-to-npm', ['release:npm']);
    grunt.registerTask('-to-git', ['release:add:commit:push']);
    grunt.registerTask('-tag',    ['release:tag:pushTags']);
 
    'patch minor major'.split(' ').forEach(function (type) {
-      grunt.registerTask(type, ['test', 'release:bump:' + type, '-tag', '-to-git', '-to-npm']);
+      grunt.registerTask(type, ['test', 'copy:typings', 'release:bump:' + type, '-tag', '-to-git', '-to-npm']);
    });
 
    grunt.registerTask('default', ['patch']);
