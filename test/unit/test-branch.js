892f307bbc89a9c78c54284587534073f7ecf2ec
Steve King <steve@mydev.co>
2017-05-06 17:28:16 +0100

    Destructured setup for ease of reading

--- test/unit/test-branch.js
+++ test/unit/test-branch.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const setup = require('./include/setup');
+const {theCommandRun, restore, Instance, closeWith} = require('./include/setup');
 const sinon = require('sinon');
 const BranchSummary = require('../../src/responses/BranchSummary');
 const BranchDeleteSummary = require('../../src/responses/BranchDeleteSummary');
@@ -8,16 +8,16 @@ const BranchDeleteSummary = require('../../src/responses/BranchDeleteSummary');
 var git, sandbox;
 
 function branchDeleteLog (branchName) {
-  return 'Deleted branch ' + branchName + ' (was b190102).';
+  return `Deleted branch ${branchName} (was b190102).`;
 }
 
 function testBranchDelete (test, options, err, branchSummary) {
     test.ok(
-      branchSummary instanceof BranchDeleteSummary, 
+      branchSummary instanceof BranchDeleteSummary,
       'Uses the BranchDeleteSummary response type'
     );
     test.equals(null, err);
-    test.same(['branch'].concat(options), setup.theCommandRun());
+    test.same(['branch'].concat(options), theCommandRun());
     test.equals('new-branch', branchSummary.branch);
     test.equals('b190102', branchSummary.hash);
     test.equals(true, branchSummary.success);
@@ -25,20 +25,20 @@ function testBranchDelete (test, options, err, branchSummary) {
 }
 
 exports.setUp = function (done) {
-   setup.restore();
+   restore();
    sandbox = sinon.sandbox.create();
    done();
 };
 
 exports.tearDown = function (done) {
-   setup.restore();
+   restore();
    sandbox.restore();
    done();
 };
 
 exports.branch = {
    setUp: function (done) {
-      git = setup.Instance();
+      git = Instance();
       done();
    },
 
@@ -49,7 +49,7 @@ exports.branch = {
 
      git.branch(options, callback);
 
-     setup.closeWith(branchDeleteLog(branchName));
+     closeWith(branchDeleteLog(branchName));
    },
 
    'delete local branch with -D option': function (test) {
@@ -59,7 +59,7 @@ exports.branch = {
 
      git.branch(options, callback);
 
-     setup.closeWith(branchDeleteLog(branchName));
+     closeWith(branchDeleteLog(branchName));
    },
 
    'delete local branch with --delete option': function (test) {
@@ -69,7 +69,7 @@ exports.branch = {
 
      git.branch(options, callback);
 
-     setup.closeWith(branchDeleteLog(branchName));
+     closeWith(branchDeleteLog(branchName));
    },
 
    'delete local branch with #deleteLocalBranch': function (test) {
@@ -78,24 +78,24 @@ exports.branch = {
 
       git.deleteLocalBranch(branchName, callback);
 
-      setup.closeWith(branchDeleteLog(branchName));
+      closeWith(branchDeleteLog(branchName));
    },
 
    'delete local branch errors': function (test) {
       git.deleteLocalBranch('new-branch', function (err, branchSummary) {
          test.ok(
-           branchSummary instanceof BranchDeleteSummary, 
+           branchSummary instanceof BranchDeleteSummary,
            'Uses the BranchDeleteSummary response type'
          );
          test.equals(null, err);
-         test.same(['branch', '-d', 'new-branch'], setup.theCommandRun());
+         test.same(['branch', '-d', 'new-branch'], theCommandRun());
          test.equals('new-branch', branchSummary.branch);
          test.equals(null, branchSummary.hash);
          test.equals(false, branchSummary.success);
          test.done();
       });
 
-      setup.closeWith('error: branch \'new-branch\' not found.');
+      closeWith('error: branch \'new-branch\' not found.');
    },
 
    'detached branches': function (test) {
@@ -142,20 +142,20 @@ exports.branch = {
 
    'gets branch with options array': function (test) {
       git.branch(['-v', '--sort=-committerdate'], function (err, data) {
-         test.same(['branch', '-v', '--sort=-committerdate'], setup.theCommandRun());
+         test.same(['branch', '-v', '--sort=-committerdate'], theCommandRun());
          test.done();
       });
 
-      setup.closeWith('');
+      closeWith('');
    },
 
    'gets branch with options object': function (test) {
       git.branch({ '-v': null, '--sort': '-committerdate'}, function (err, data) {
-         test.same(['branch', '-v', '--sort=-committerdate'], setup.theCommandRun());
+         test.same(['branch', '-v', '--sort=-committerdate'], theCommandRun());
          test.done();
       });
 
-      setup.closeWith('');
+      closeWith('');
    },
 
    'gets branch data': function (test) {
@@ -171,7 +171,7 @@ exports.branch = {
          test.done();
       });
 
-      setup.closeWith('\
+      closeWith('\
   cflynn07-add-git-ignore            a0b67a3 Add support for filenames containing spaces\n\
 * drschwabe-add-branches             063069b Merge branch \'add-branches\' of https://github.com/drschwabe/git-js into drschwabe-add-branches\n\
   master                             cb4be06 Release 1.30.0\n\
@@ -183,10 +183,10 @@ exports.branch = {
          test.ok(branchSummary instanceof BranchSummary, 'Uses the BranchSummary response type');
          test.equals(null, err, 'not an error');
          test.same(['master'], branchSummary.all);
-         test.same(['branch', '-v'], setup.theCommandRun());
+         test.same(['branch', '-v'], theCommandRun());
          test.done();
       });
-       setup.closeWith('\
+       closeWith('\
 * master                899725c [ahead 1] Add clean method\n\
   remotes/origin/HEAD   -> origin/master\n\
         ');
