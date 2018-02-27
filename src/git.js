50c0330ca03820abb9a776eb6ca76127dde6c54b
René Rössler <rene@freshx.de>
2018-02-27 18:10:53 +0100

    Use close event instead of exit event as it also works on very fast machines. See https://stackoverflow.com/questions/26446791/node-read-spawn-stderr-stdout-after-quick-exit-event

--- src/git.js
+++ src/git.js
@@ -1377,7 +1377,7 @@ Please switch to using Git#exec to run arbitrary functions as part of the comman
             stdErr.push(new Buffer(err.stack, 'ascii'));
          });
 
-         spawned.on('exit', function (exitCode, exitSignal) {
+         spawned.on('close', function (exitCode, exitSignal) {
             function done (output) {
                then.call(git, null, output);
             }
