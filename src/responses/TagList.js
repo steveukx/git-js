22a121f97538a269cf94451939c7c395ab9894ab
Steve King <steve@mydev.co>
2020-04-10 06:35:48 +0100

    Support for Node 12 in `git tags` response list ordering Switch tests to use `jest` with the `jestify-node-unit` wrapper to make use of a real test runner

--- src/responses/TagList.js
+++ src/responses/TagList.js
@@ -16,27 +16,24 @@ TagList.parse = function (data, customSort) {
    };
 
    var tags = data
-      .trim()
       .split('\n')
-      .map(function (item) { return item.trim(); })
+      .map(trimmed)
       .filter(Boolean);
 
    if (!customSort) {
       tags.sort(function (tagA, tagB) {
-         var partsA = tagA.split('.');
-         var partsB = tagB.split('.');
+         const partsA = tagA.split('.');
+         const partsB = tagB.split('.');
 
          if (partsA.length === 1 || partsB.length === 1) {
-            return tagA - tagB > 0 ? 1 : -1;
+            return singleSorted(partsA[0], partsB[0]);
          }
 
-         for (var i = 0, l = Math.max(partsA.length, partsB.length); i < l; i++) {
-            var a = number(partsA[i]);
-            var b = number(partsB[i]);
+         for (let i = 0, l = Math.max(partsA.length, partsB.length); i < l; i++) {
+            const diff = sorted(toNumber(partsA[i]), toNumber(partsB[i]));
 
-            var diff = a - b;
             if (diff) {
-               return diff > 0 ? 1 : -1;
+               return diff;
             }
          }
 
@@ -48,3 +45,30 @@ TagList.parse = function (data, customSort) {
 
    return new TagList(tags, latest);
 };
+
+function singleSorted (a, b) {
+   const aIsNum = isNaN(a);
+   const bIsNum = isNaN(b);
+
+   if (aIsNum !== bIsNum) {
+      return aIsNum ? 1 : -1;
+   }
+
+   return sorted(a, b);
+}
+
+function sorted (a, b) {
+   return a === b ? 0 : a > b ? 1 : -1;
+}
+
+function trimmed (input) {
+   return input.trim();
+}
+
+function toNumber (input) {
+   if (typeof input === 'string') {
+      return parseInt(input.replace(/^\D+/g, ''), 10) || 0;
+   }
+
+   return 0;
+}
