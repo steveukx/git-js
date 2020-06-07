4ab408215c13889dd223dfb5741983fc4af2c465
Steve King <steve@mydev.co>
2020-06-07 09:03:24 +0100

    Add clean parser test

--- test/unit/clean.spec.js
+++ test/unit/clean.spec.js
@@ -1,21 +1,44 @@
-const {theCommandRun, theCommandsRun, restore, Instance, closeWith, instanceP} = require('./include/setup');
-const sinon = require('sinon');
+const {theCommandRun, theCommandsRun, restore, Instance, closeWithSuccess, instanceP, wait} = require('./include/setup');
+const {createSandbox} = require('sinon');
 
 const {TaskConfigurationError} = require('../../src/lib/errors/task-configuration-error');
-const {CleanResponse} = require("../../src/lib/responses/CleanSummary");
+const {CleanResponse, cleanSummaryParser} = require("../../src/lib/responses/CleanSummary");
 const {CleanOptions, CONFIG_ERROR_INTERACTIVE_MODE, CONFIG_ERROR_MODE_REQUIRED, CONFIG_ERROR_UNKNOWN_OPTION} = require('../../src/lib/tasks/clean');
 
 describe('clean', () => {
 
    let git, sandbox;
 
-   beforeEach(() => {
-      restore();
-      sandbox = sinon.createSandbox();
-   });
+   beforeEach(() => sandbox = createSandbox());
 
-   afterEach(() => {
-      restore(sandbox);
+   afterEach(() => restore(sandbox));
+
+   describe('parser', () => {
+
+      function parserTest (dryRun, prefix) {
+         const summary = cleanSummaryParser(dryRun, `
+            ${prefix} a
+            ${prefix} b/
+            ${prefix} c
+         `);
+
+         expect(summary).toBeInstanceOf(CleanResponse);
+         expect(summary).toEqual(expect.objectContaining({
+            dryRun,
+            paths: ['a', 'b/', 'c'],
+            files: ['a', 'c'],
+            folders: ['b/'],
+         }));
+      }
+
+      it('recognises items in dry run', () => {
+         parserTest(true, 'Would remove');
+      });
+
+      it('recognises items in force', () => {
+         parserTest(false, 'Removing');
+      });
+
    });
 
    describe('promises', () => {
@@ -23,7 +46,7 @@ describe('clean', () => {
 
       it('cleans', async () => {
          const cleanedP = git.clean(CleanOptions.FORCE);
-         closeWith(`
+         await closeWithSuccess(`
             Removing a
             Removing b/
          `);
@@ -38,32 +61,25 @@ describe('clean', () => {
       });
 
       it('options combined as a string', async () => {
-         closeWith('');
+         closeWithSuccess();
          await git.clean(CleanOptions.FORCE + CleanOptions.RECURSIVE);
          expect(theCommandRun()).toEqual(['clean', '-f', '-d']);
       });
 
       it('cleans multiple paths', async () => {
-         closeWith('');
+         closeWithSuccess();
          await git.clean(CleanOptions.FORCE, ['./path-1', './path-2']);
          expect(theCommandRun()).toEqual(['clean', '-f', './path-1', './path-2']);
       });
 
       it('cleans with options and multiple paths', async () => {
-         closeWith('');
+         closeWithSuccess();
          await git.clean(CleanOptions.IGNORED_ONLY + CleanOptions.FORCE, {'./path-1': null, './path-2': null});
          expect(theCommandRun()).toEqual(['clean', '-f', '-X', './path-1', './path-2']);
       });
 
       it('handles configuration errors', async () => {
-         let err;
-         try {
-            closeWith('');
-            await git.clean('X');
-         }
-         catch (e) {
-            err = e;
-         }
+         const err = await (git.clean('X').catch(e => e));
 
          expectTheError(err).toBe(CONFIG_ERROR_MODE_REQUIRED);
       });
@@ -74,106 +90,100 @@ describe('clean', () => {
 
       beforeEach(() => git = Instance());
 
-      it('cleans with dfx', () => new Promise((done) => {
-         git.clean('dfx', function (err, data) {
+      it('cleans with dfx', test((done) => {
+         git.clean('dfx', function (err) {
             expect(err).toBeNull();
             expect(theCommandRun()).toEqual(['clean', '-f', '-d', '-x']);
             done();
          });
-         closeWith('');
+         closeWithSuccess();
       }));
 
-      it('missing required n or f in mode', () => new Promise((done) => {
+      it('missing required n or f in mode', test((done) => {
          git.clean('x', function (err) {
             expectTheError(err).toBe(CONFIG_ERROR_MODE_REQUIRED);
             expectNoTasksToHaveBeenRun();
             done();
          });
-         closeWith('');
       }));
 
-      it('unknown options', () => new Promise((done) => {
-         git.clean('fa', function (err, data) {
+      it('unknown options', test((done) => {
+         git.clean('fa', function (err) {
             expectTheError(err).toBe(CONFIG_ERROR_UNKNOWN_OPTION);
             expectNoTasksToHaveBeenRun();
             done();
          });
-         closeWith('');
       }));
 
-      it('no args', () => new Promise((done) => {
-         git.clean(function (err, data) {
+      it('no args', test((done) => {
+         git.clean(function (err) {
             expectTheError(err).toBe(CONFIG_ERROR_MODE_REQUIRED);
             expectNoTasksToHaveBeenRun();
             done();
          });
-         closeWith('');
       }));
 
-      it('just show no directories', () => new Promise((done) => {
+      it('just show no directories', test((done) => {
          git.clean('n', function (err) {
             expect(err).toBeNull();
             expect(theCommandRun()).toEqual(['clean', '-n']);
             done();
          });
-         closeWith('');
+         closeWithSuccess();
       }));
 
-      it('just show', () => new Promise((done) => {
+      it('just show', test((done) => {
          git.clean('n', ['-d'], function (err) {
             expect(err).toBeNull();
             expect(theCommandRun()).toEqual(['clean', '-n', '-d']);
             done();
          });
-         closeWith('Would remove install.js');
+         closeWithSuccess('Would remove install.js');
       }));
 
-      it('force clean space', () => new Promise((done) => {
+      it('force clean space', test((done) => {
          git.clean('f', ['-d'], function (err) {
             expect(err).toBeNull();
             expect(theCommandRun()).toEqual(['clean', '-f', '-d']);
             done();
          });
-         closeWith('');
+         closeWithSuccess();
       }));
 
-      it('clean ignored files', () => new Promise((done) => {
+      it('clean ignored files', test((done) => {
          git.clean('f', ['-x', '-d'], function (err) {
             expect(err).toBeNull();
             expect(theCommandRun()).toEqual(['clean', '-f', '-x', '-d']);
             done();
          });
-         closeWith('');
+         closeWithSuccess();
       }));
 
-      it('prevents interactive mode - shorthand option', () => new Promise((done) => {
+      it('prevents interactive mode - shorthand option', test((done) => {
          git.clean('f', ['-i'], function (err) {
             expectTheError(err).toBe(CONFIG_ERROR_INTERACTIVE_MODE);
             expectNoTasksToHaveBeenRun();
 
             done();
          });
-         closeWith('');
       }));
 
-      it('prevents interactive mode - shorthand mode', () => new Promise((done) => {
+      it('prevents interactive mode - shorthand mode', test((done) => {
          git.clean('fi', function (err) {
             expectTheError(err).toBe(CONFIG_ERROR_INTERACTIVE_MODE);
             expectNoTasksToHaveBeenRun();
 
             done();
          });
-         closeWith('');
       }));
 
-      it('prevents interactive mode - longhand option', () => new Promise((done) => {
+      it('prevents interactive mode - longhand option', test((done) => {
          git.clean('f', ['--interactive'], function (err) {
             expectTheError(err).toBe(CONFIG_ERROR_INTERACTIVE_MODE);
             expectNoTasksToHaveBeenRun();
 
             done();
          });
-         closeWith('');
       }));
    });
 
@@ -188,6 +198,13 @@ describe('clean', () => {
             expect(err.toString()).toMatch(message);
          }
       }
+   }
+
+   function test (t) {
+      return async () => {
+         const result = await (new Promise(done => t(done)));
+         await wait();
+      };
    }
 
 });
