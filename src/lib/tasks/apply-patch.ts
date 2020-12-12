3303a531f9525bfcb3fad811f48e229afc6240a6
Andre Terron <git@terron.me>
2020-12-11 20:57:34 -0800

    Add the ApplyOptions type back

--- src/lib/tasks/apply-patch.ts
+++ src/lib/tasks/apply-patch.ts
@@ -1,5 +1,40 @@
 import { straightThroughStringTask } from "./task";
-import { StringTask } from "../types";
+import { Options, OptionFlags, StringTask } from "../types";
+
+export type ApplyOptions = Options &
+   OptionFlags<
+      | "--stat"
+      | "--numstat"
+      | "--summary"
+      | "--check"
+      | "--index"
+      | "--intent-to-add"
+      | "--3way"
+      | "--apply"
+      | "--no-add"
+      | "-R"
+      | "--reverse"
+      | "--allow-binary-replacement"
+      | "--binary"
+      | "--reject"
+      | "-z"
+      | "--inaccurate-eof"
+      | "--recount"
+      | "--cached"
+      | "--ignore-space-change"
+      | "--ignore-whitespace"
+      | "--verbose"
+      | "--unsafe-paths"
+   > &
+   OptionFlags<
+      "--whitespace",
+      "nowarn" | "warn" | "fix" | "error" | "error-all"
+   > &
+   OptionFlags<
+      "--build-fake-ancestor" | "--exclude" | "--include" | "--directory",
+      string
+   > &
+   OptionFlags<"-p" | "-C", number>;
 
 export function applyPatchTask(
    patches: string | string[],
