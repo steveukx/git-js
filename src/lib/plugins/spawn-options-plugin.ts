116374db806fc3f1254e3f52c55e72c2f19d770c
Steve King <steve@mydev.co>
2021-06-12 10:49:25 +0100

    Lint

--- src/lib/plugins/spawn-options-plugin.ts
+++ src/lib/plugins/spawn-options-plugin.ts
@@ -3,12 +3,12 @@ import { pick } from '../utils';
 import { SimpleGitPlugin } from './simple-git-plugin';
 
 export function spawnOptionsPlugin(spawnOptions: Partial<SpawnOptions>): SimpleGitPlugin<'spawn.options'> {
-  const options = pick(spawnOptions, ['uid', 'gid']);
+   const options = pick(spawnOptions, ['uid', 'gid']);
 
-  return {
-     type: 'spawn.options',
-     action(data) {
-        return {...options, ...data};
-     },
-  };
+   return {
+      type: 'spawn.options',
+      action(data) {
+         return {...options, ...data};
+      },
+   };
 }
