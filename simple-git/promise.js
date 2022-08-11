2ac1d3f9990ad3683e21d1b03c6679634903f374
Steve King <steve@mydev.co>
2022-08-11 08:32:57 +0100

    Remove ability to import `/promise` types and throw when required.

--- simple-git/promise.js
+++ simple-git/promise.js
@@ -1,18 +1,8 @@
-// TODO: deprecation warning
-console.warn(`=============================================
+throw new Error(`=============================================
 simple-git has supported promises / async await since version 2.6.0.
- Importing from 'simple-git/promise' has been deprecated and will be
- removed by July 2022.
+ Importing from 'simple-git/promise' was deprecated in January 2022
+ and removed in August 2022.
 
-To upgrade, change all 'simple-git/promise' imports to just 'simple-git'
+Upgrading is simple though:
+ change all 'simple-git/promise' imports to just 'simple-git'
 =============================================`);
-
-const simpleGit = require('.');
-
-module.exports = Object.assign(
-   function () {
-      return simpleGit.gitP.apply(null, arguments);
-   },
-   simpleGit,
-   { default: simpleGit.gitP }
-);
