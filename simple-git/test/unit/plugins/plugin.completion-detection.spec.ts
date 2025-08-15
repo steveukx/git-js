d562af90fe79f96817af2cdcb2d42c79b528338c
Mat Dodgson <mat@dodgson.me>
2025-08-15 14:36:35 +1000

    Remove on exit

--- simple-git/test/unit/plugins/plugin.completion-detection.spec.ts
+++ simple-git/test/unit/plugins/plugin.completion-detection.spec.ts
@@ -2,13 +2,12 @@ import { newSimpleGit, theChildProcessMatching, wait } from '../__fixtures__';
 import { MockChildProcess } from '../__mocks__/mock-child-process';
 
 describe('completionDetectionPlugin', () => {
-   function process(proc: MockChildProcess, data: string, close = false, exit = false) {
+   function process(proc: MockChildProcess, data: string, close = false) {
       proc.stdout.$emit('data', Buffer.from(data));
       close && proc.$emit('close', 1);
-      exit && proc.$emit('exit', 1);
    }
 
-   it('can respond to just close events', async () => {
+   it('can respond to close events', async () => {
       const git = newSimpleGit({
          completion: {
             onClose: true,
@@ -20,27 +19,9 @@ describe('completionDetectionPlugin', () => {
 
       await wait();
 
-      process(theChildProcessMatching(['foo']), 'foo', false, true);
-      process(theChildProcessMatching(['bar']), 'bar', true, false);
+      process(theChildProcessMatching(['foo']), 'foo', false);
+      process(theChildProcessMatching(['bar']), 'bar', true);
 
       expect(await output).toBe('bar');
-   });
-
-   it('can respond to just exit events', async () => {
-      const git = newSimpleGit({
-         completion: {
-            onClose: false,
-            onExit: true,
-         },
-      });
-
-      const output = Promise.race([git.raw('foo'), git.raw('bar')]);
-
-      await wait();
-
-      process(theChildProcessMatching(['foo']), 'foo', false, true);
-      process(theChildProcessMatching(['bar']), 'bar', true, false);
-
-      expect(await output).toBe('foo');
    });
 });
