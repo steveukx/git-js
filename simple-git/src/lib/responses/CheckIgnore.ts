240ec6411e81770eb3b20dbddfe125349e213f97
Steve King <steve@mydev.co>
2025-10-28 16:21:39 +0000

    Add test for absolute paths in checkIgnore (#1085)
    * Normalize paths returned from `git.checkIgnore`
    * Strip surrounding quotes from the raw `git check-ignore` response for `path.normalize` to recognise when windows paths are absolute.

--- simple-git/src/lib/responses/CheckIgnore.ts
+++ simple-git/src/lib/responses/CheckIgnore.ts
@@ -1,9 +1,13 @@
+import { normalize } from 'node:path';
+
 /**
  * Parser for the `check-ignore` command - returns each file as a string array
  */
 export const parseCheckIgnore = (text: string): string[] => {
-   return text
-      .split(/\n/g)
-      .map((line) => line.trim())
-      .filter((file) => !!file);
+   return text.split(/\n/g).map(toPath).filter(Boolean);
 };
+
+function toPath(input: string) {
+   const path = input.trim().replace(/^["']|["']$/g, '');
+   return path && normalize(path);
+}
--- simple-git/test/integration/check-ignore.win32.spec.ts
+++ simple-git/test/integration/check-ignore.win32.spec.ts
@@ -1,4 +1,5 @@
 import { createTestContext, setUpIgnored, setUpInit, SimpleGitTestContext } from '@simple-git/test-utils';
+import { join } from 'node:path';
 
 describe('checkIgnore', () => {
 
@@ -10,7 +11,7 @@ describe('checkIgnore', () => {
       await setUpIgnored(context, ['ignored', 'partially/untracked']);
    });
 
-   it('detects ignored files', async () => {
+   it('detects ignored files - relative paths', async () => {
       const actual = await context.git.checkIgnore([
          'ignored/anything',
          'tracked/anything',
@@ -19,8 +20,33 @@ describe('checkIgnore', () => {
       ]);
 
       expect(actual).toEqual([
-         'ignored/anything',
-         'partially/untracked/file',
+         join('ignored', 'anything'),
+         join('partially','untracked', 'file'),
       ]);
+   });
+
+   it('detects ignored files - absolute paths', async () => {
+      const paths = [
+         join(context.root, 'ignored', 'anything'),
+         join(context.root, 'tracked', 'anything'),
+         join(context.root, 'partially', 'tracked'),
+         join(context.root, 'partially', 'untracked', 'file'),
+      ];
+      const actual = await context.git.checkIgnore(paths);
+
+      expect(actual).toEqual([
+         paths[0],
+         paths[3],
+      ]);
+   });
+
+   it('detects ignored files - absolute and relative paths', async () => {
+      const paths = [
+         join(context.root, 'ignored', 'anything'),
+         join('partially', 'untracked', 'file'),
+      ];
+      const actual = await context.git.checkIgnore(paths);
+
+      expect(actual).toEqual(paths);
    });
 });
