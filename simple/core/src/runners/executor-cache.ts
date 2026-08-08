af01307f025a88dd91f013ec529c53550027b947
Steve King <steve@mydev.co>
2026-08-08 17:15:52 +0100

    Switch to `WeakMap` registry of git instances in favour of pseudo-private member variables for the `_executor`

--- simple/core/src/runners/executor-cache.ts
+++ simple/core/src/runners/executor-cache.ts
@@ -3,11 +3,11 @@ import type { SimpleGitExecutor } from '../types';
 
 const ExecutorCache = new WeakMap<TaskMethods, SimpleGitExecutor>();
 
-export function setExecutor(instance: TaskMethods, executor: SimpleGitExecutor) {
+export function setExecutor(instance: TaskMethods, executor: SimpleGitExecutor): void {
    ExecutorCache.set(instance, executor);
 }
 
-export function getExecutor(simpleGit: TaskMethods) {
+export function getExecutor(simpleGit: TaskMethods): SimpleGitExecutor {
    const executor = ExecutorCache.get(simpleGit);
    if (!executor) {
       throw new Error(`getExecutor(): no registered executor available - GC or Out of Memory`);
