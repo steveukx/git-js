7189a9e709df958636444aa5e76e01b1a7ccabe6
Steve King <steve@mydev.co>
2026-09-02 21:20:39 +0100

    Use pnpm in build scripts

--- devtools/package-versions.ts
+++ devtools/package-versions.ts
@@ -1,9 +1,4 @@
-import { readFile } from 'fs/promises';
-import { resolve } from 'path';
-
 import { execP } from '@kwsites/exec-p';
-
-import { repoRoot } from './repo-root';
 
 const versions = getVersions();
 
@@ -16,27 +11,9 @@ export const getWorkspaceVersion = async (packageName: string) => {
 };
 
 async function getVersions(): Promise<Record<string, string>> {
-   const workspaces = (await execP('yarn', ['workspaces', 'list', '--json']))
-      .split('\n')
-      .map((line) => {
-         try {
-            return (line && JSON.parse(line)) || null;
-         } catch (e) {
-            console.error(`getVersions(): error: `, e);
-            return null;
-         }
-      })
-      .filter(Boolean)
-      .map(({ name, location }) => [name, resolve(repoRoot, location, 'package.json')]);
+   const workspaces = JSON.parse(
+      await execP('pnpm', ['list', '-r', '--depth', '-1', '--json'])
+   ).map(({ name, version }) => [name, version]);
 
-   const versions: Record<string, string> = {};
-   for (const [name, location] of workspaces) {
-      versions[name] = await getVersion(location);
-   }
-
-   return versions;
-}
-
-async function getVersion(path: string) {
-   return String(JSON.parse(await readFile(path, 'utf8')).version);
+   return Object.fromEntries(workspaces);
 }
