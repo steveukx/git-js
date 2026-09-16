7bb2c1c3d19c6507ef053e1eec01b1a58cea5dea
test <steve@mydev.co>
2026-09-16 15:15:25 +0300

    - support `build:pkg` only modifying the `export` publish block, rather than full removal of `scripts` and `devDependencies`. - `typescript-esm-consumer` package updated to build ESM JavaScript (includes type check) then run directly with node (includes validation of exported functionality).

---
+++ packages/test-typescript-esm-consumer/test-types/named-imports.types.ts
@@ -0,0 +1,53 @@
+import {CleanOptions, simpleGit, type SimpleGit, TaskConfigurationError} from 'simple-git';
+
+export async function testSingleReturn() {
+   expect(await simpleGit().checkIsRepo()).toBe(true);
+}
+
+export async function testErrorConstructor() {
+   expect(new TaskConfigurationError('foo')).toBeInstanceOf(TaskConfigurationError);
+}
+
+export async function testEnumExport() {
+   expect(CleanOptions.DRY_RUN === 'n').toBe(true);
+}
+
+export async function testInterfaceExport() {
+   const git: SimpleGit = simpleGit();
+   expect(git).toBeDefined();
+}
+
+function expect<T>(expected: T) {
+   return {
+      toBe(actual: T) {
+         if (actual !== expected) {
+            throw new Error(`Expected ${expected} to be ${actual}`);
+         }
+      },
+      toBeInstanceOf(actual: new (...args: never[]) => T) {
+         if ( !(expected instanceof actual )) {
+            throw new Error(`Expected ${expected} to be instanceof ${actual}`);
+         }
+      },
+      toBeDefined() {
+         if (expected === undefined) {
+            throw new Error(`Expected ${expected} not to be undefined`);
+         }
+      }
+   }
+}
+
+(async function (...tests: Array<() => Promise<void>>) {
+   await tests.reduce((chain, test) => {
+      return chain.then(() => {
+         console.log(`Running ${test.name}`);
+         test();
+      })
+   }, Promise.resolve());
+   console.log(`Tests complete`);
+}(
+   testSingleReturn,
+   testErrorConstructor,
+   testEnumExport,
+   testInterfaceExport
+));
