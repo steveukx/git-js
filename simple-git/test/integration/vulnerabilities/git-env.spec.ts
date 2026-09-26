c7bf4d0051598b63831a1bfebfcf0c436c5b5306
test <steve@mydev.co>
2026-09-26 15:34:22 +0300

    Lint

--- simple-git/test/integration/vulnerabilities/git-env.spec.ts
+++ simple-git/test/integration/vulnerabilities/git-env.spec.ts
@@ -1,7 +1,7 @@
-import {describe, expect, it} from 'vitest';
-import {exists, FILE} from '@kwsites/file-exists';
-import {promiseError} from '@kwsites/promise-result';
-import {assertGitError, createTestContext, newSimpleGit, setUpInit} from '@simple-git/test-utils';
+import { describe, expect, it } from 'vitest';
+import { exists, FILE } from '@kwsites/file-exists';
+import { promiseError } from '@kwsites/promise-result';
+import { assertGitError, createTestContext, newSimpleGit, setUpInit } from '@simple-git/test-utils';
 
 describe('--exec-path', () => {
    it('blocks: use of VISUAL blocked by default', async () => {
@@ -14,7 +14,11 @@ describe('--exec-path', () => {
       await newSimpleGit(root).raw('add', '.');
       await newSimpleGit(root).raw('commit', '-m', 'foo');
 
-      const err = await promiseError(newSimpleGit(root).env({ VISUAL: `touch ${pwnd}` }).raw('commit', '--ammend'));
+      const err = await promiseError(
+         newSimpleGit(root)
+            .env({ VISUAL: `touch ${pwnd}` })
+            .raw('commit', '--ammend')
+      );
 
       expect(exists(pwnd, FILE)).toBe(false);
 
